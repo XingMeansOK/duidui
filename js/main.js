4a2a2d5c6f6c61847c05064e840497d6b15f3ba8
XingMeansOK <771267736@qq.com>
2018-08-05 23:56:49 +0800

    创建基本场景：地面，天空，灯光，阴影

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // import Player from './player/index'
 // import Enemy from './npc/enemy'
 // import BackGround from './runtime/background'
@@ -5,8 +6,8 @@
 // import Music from './runtime/music'
 // import DataBus from './databus'
 // import THREE from './libs/three.min.js'
+import World from './runtime/world'
 let THREE = require('./libs/three.min.js')
-
 // let ctx = canvas.getContext('2d')
 // let databus = new DataBus()
 
@@ -184,31 +185,136 @@ class Main {
  * 游戏主函数
  */
 export default class _3D {
-  constructor() {
+  _constructor() {
     // 场景
     this.scene = new THREE.Scene()
-    // 透视摄像头
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    this.camera.position.z = 2.5
-    this.camera.position.y = 2.5
-    this.camera.lookAt(0, 0, 0)
+    this.scene.background = new THREE.Color('skyblue')
+    // 正射摄像头
+    let aspect = window.innerWidth / window.innerHeight
+    this.camera = new THREE.OrthographicCamera(- 20 * aspect, 20 * aspect, 20, - 20, 0.1, 1000);
+    // this.camera = new THREE.OrthographicCamera(- 2 * aspect, 2 * aspect, 2, - 2, 0.1, 1000);
+    this.camera.position.z = 1
+    this.camera.position.y = 10.5
+    this.camera.lookAt(0, 10, 0)
+    this.camera.position.y = 1.5
+    this.camera.lookAt(0, 1, 0)
     // webGL渲染器
     // 同时指定canvas为小游戏暴露出来的canvas
     this.renderer = new THREE.WebGLRenderer({canvas})
     this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.world = new World()
+    this.scene.add(this.world)
     var geometry = new THREE.CubeGeometry(1, 1, 1)
-    var material = new THREE.MeshBasicMaterial()
+    var material = new THREE.MeshBasicMaterial({ color: 0xffffff, specular: 0x050505 })
     this.cube = new THREE.Mesh(geometry, material)
-    this.scene.add(this.cube)
-
     this.cube.castShadow = true
+    this.world.add(this.cube)
+
+   
+
+    window.requestAnimationFrame(this.loop.bind(this), canvas)
+  }
+
+  constructor() {
+    var camera, scene, renderer, dirLight, dirLightHeper, hemiLight, hemiLightHelper;
+
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 5000);
+    camera.position.set(0, 0, 500);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color().setHSL(0.6, 0, 1);
+    scene.fog = new THREE.Fog(scene.background, 1, 5000);
+    // LIGHTS
+    hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+    hemiLight.color.setHSL(0.6, 1, 0.6);
+    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+    hemiLight.position.set(0, 50, 0);
+    scene.add(hemiLight);
+    hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+    scene.add(hemiLightHelper);
+    //
+    dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.color.setHSL(0.1, 1, 0.95);
+    dirLight.position.set(-1, 1.75, 1);
+    dirLight.position.multiplyScalar(30);
+    scene.add(dirLight);
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+    var d = 50;
+    dirLight.shadow.camera.left = -d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = -d;
+    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.bias = -0.0001;
+    dirLightHeper = new THREE.DirectionalLightHelper(dirLight, 10);
+    scene.add(dirLightHeper);
+    // GROUND
+    var groundGeo = new THREE.PlaneBufferGeometry(4000, 4000);
+    var groundMat = new THREE.MeshPhongMaterial({ color: 0xff00f0, specular: 0x050505 }); 
+    groundMat.color.setHSL(0.095, 1, 0.75);
+    var ground = new THREE.Mesh(groundGeo, groundMat);
+    ground.rotation.x = -Math.PI / 2;
+    ground.position.y = -33;
+    scene.add(ground);
+    ground.receiveShadow = true;
+
+    var vertexShader = `varying vec3 vWorldPosition;
+			void main() {
+				vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+				vWorldPosition = worldPosition.xyz;
+				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+			}`;
+    var fragmentShader = `
+    			uniform vec3 topColor;
+			uniform vec3 bottomColor;
+			uniform float offset;
+			uniform float exponent;
+			varying vec3 vWorldPosition;
+			void main() {
+				float h = normalize( vWorldPosition + offset ).y;
+				gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h , 0.0), exponent ), 0.0 ) ), 1.0 );
+			}
+    `;
+    var uniforms = {
+      topColor: { value: new THREE.Color(0x0077ff) },
+      bottomColor: { value: new THREE.Color(0xffffff) },
+      offset: { value: 33 },
+      exponent: { value: 0.6 }
+    };
+    uniforms.topColor.value.copy(hemiLight.color);
+    scene.fog.color.copy(uniforms.bottomColor.value);
+    var skyGeo = new THREE.SphereBufferGeometry(2000, 32, 15);
+    var skyMat = new THREE.ShaderMaterial({ vertexShader: vertexShader, fragmentShader: fragmentShader, uniforms: uniforms, side: THREE.BackSide });
+    var sky = new THREE.Mesh(skyGeo, skyMat);
+    scene.add(sky);
+
+
+    var geometry = new THREE.CubeGeometry(10, 10, 10)
+    var material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+    this.cube = new THREE.Mesh(geometry, material)
+    this.cube.castShadow = true
+    this.cube.position.y = -28
+    scene.add(this.cube)
+
+    renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    renderer.shadowMap.enabled = true;
+
+    this.scene = scene
+    this.camera = camera
+    this.renderer = renderer
+
+
+
     window.requestAnimationFrame(this.loop.bind(this), canvas)
   }
 
   update() {
-    // this.cube.rotation.x += 0.02;
-    // this.cube.rotation.y += 0.04;
-    // this.cube.rotation.z += 0.06;
+    // System.rotation(this.rotation)
   }
   loop() {
     this.update()
